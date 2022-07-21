5f0111c670a2cc8505e68ebe85e90ea91aff3e00
Cheng Yi <cenzhedong2@126.com>
2022-07-21 20:35:00 +0800

    修改路径

--- examples/task_seq2seq_autotitle.py
+++ examples/task_seq2seq_autotitle.py
@@ -21,12 +21,12 @@ steps_per_epoch = 1000
 epochs = 10000
 
 # bert配置
-config_path = '~/nas/dataset/chinese_L-12_H-768_A-12/bert_config.json'
-checkpoint_path = '~/nas/dataset/chinese_L-12_H-768_A-12/bert_model.ckpt'
-dict_path = '~/nas/dataset/chinese_L-12_H-768_A-12/vocab.txt'
+config_path = '/home/cenzhedong/nas/dataset/chinese_L-12_H-768_A-12/bert_config.json'
+checkpoint_path = '/home/cenzhedong/nas/dataset/chinese_L-12_H-768_A-12/bert_model.ckpt'
+dict_path = '/home/cenzhedong/nas/dataset/chinese_L-12_H-768_A-12/vocab.txt'
 
 # 训练样本。THUCNews数据集，每个样本保存为一个txt。
-txts = glob.glob('~/nas/dataset/THUCNews/*/*.txt')
+txts = glob.glob('/home/cenzhedong/nas/dataset/THUCNews/*/*.txt')
 
 # 加载并精简词表，建立分词器
 token_dict, keep_tokens = load_vocab(
